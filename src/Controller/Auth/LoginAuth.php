5937ff66e2467ac4b44cd51e04f2b0ddb3617a22
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-09 00:26:18 +0800

    remaned

---
+++ src/Controller/Auth/LoginAuth.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Controller\Auth;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserInfo {
+    public $name;
+    public $lastName;
+    public $userName;
+    
+    public function setCred($name,$lastname,$username){
+        $this->name = $name;
+        $this->lastName = $lastname;
+        $this->userName = $username;
+    }
+    public function whoami($name,$lastname){
+        return "I am " . $name . " " . $lastname;
+    }
+};
+
+class LoginAuth extends AbstractController{
+    #[Route('auth/login-auth',name:"login-auth")]
+    public function doLoginAuth(Request $request){
+        $user = new UserInfo();
+        $data = json_decode($request->getContent(), true);
+        $username = $data['username'] ?? '';
+        $lastname = $data['password'] ?? '';
+        
+        // $user->whoami($username,$lastname);
+
+        $message = $user->whoami($username, $lastname);
+        return new JsonResponse([
+            'success'=>true,
+            'message'=> $message
+        ]);
+
+    }
+}
--- src/Controller/Auth/UserLogin.php
+++ src/Controller/Auth/UserLogin.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Controller\Pages;
+namespace App\Controller\Auth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
