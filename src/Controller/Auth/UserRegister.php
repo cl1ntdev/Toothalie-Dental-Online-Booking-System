dc89c592fdd8642a9621398447e7aca6d3d7af90
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-11 23:12:15 +0800

    user Register page

---
+++ src/Controller/Auth/UserRegister.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Controller\API;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserRegister extends AbstractController {
+    #[Route("/pages/register",name:"user-register")]
+    public function doUserRegister(){
+        return $this->render('Pages/user_register.html.twig');
+    }
+}
