6b44885fe100142628a628ae1730debe00abca43
cl1ntdev <clintjayestrellanes17@gmail.com>
2025-09-12 19:34:24 +0800

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+    "./src/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
